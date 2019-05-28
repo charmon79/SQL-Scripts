f3d595848889ada8c16a81a62b701e0d2e655b73
charmon79 <charmon79@gmail.com>
2019-05-28 09:44:03 -0500

    ignore system tables, include rows_sampled

--- Statistics find outdated.sql
+++ Statistics find outdated.sql
@@ -1,4 +1,4 @@
-USE LME;
+USE cadnceprd;
 GO
 
 SELECT
@@ -8,6 +8,8 @@ SELECT
 ,   st.name AS stats_name
 ,   stp.last_updated
 ,   stp.rows
+,   stp.rows_sampled
+,   (stp.rows_sampled / (1.0 * stp.rows)) * 100.0 AS sample_percent
 ,   stp.modification_counter
 ,   ((1.0 * stp.modification_counter) / stp.rows) * 100 AS percent_modified
 FROM
@@ -20,13 +22,14 @@ FROM
     CROSS APPLY sys.dm_db_stats_properties (o.object_id, st.stats_id) stp
 WHERE
     1=1
+    and o.is_ms_shipped = 0
     --and o.name IN (
     --    'orders'
     --)
     --and c.name IN (
     --    'company_id'
     --)
-    AND (1.0 * stp.modification_counter) / stp.rows >= 0.01 -- at least 1% of rows changed
+    --AND (1.0 * stp.modification_counter) / stp.rows >= 0.01 -- at least 1% of rows changed
 ORDER BY
     modification_counter DESC
 
